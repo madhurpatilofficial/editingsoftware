e4df01ff5b8a1be3b695b74d733dbf502ce75472
Madhur Patil <madhurpatilofficial@gmail.com>
2024-06-30 18:59:50 +0530

    New Features Added

--- src/app/components/text-editor/text-editor.component.ts
+++ src/app/components/text-editor/text-editor.component.ts
@@ -1,11 +1,19 @@
-import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef, HostListener } from '@angular/core';
 import { MatTableDataSource } from '@angular/material/table';
+import html2canvas from 'html2canvas';
+import jsPDF from 'jspdf';
+import { marked } from 'marked';
 import Swal from 'sweetalert2';
 
 export interface TableData {
   col1: string;
   col2: string;
 }
+interface Comment {
+  text: string;
+  range: Range;
+}
+declare var webkitSpeechRecognition: any;
 
 @Component({
   selector: 'app-text-editor',
@@ -13,14 +21,18 @@ export interface TableData {
   styleUrls: ['./text-editor.component.css'],
 })
 export class TextEditorComponent implements OnInit {
+comments: any;
   alert(arg0: number) {
     throw new Error('Method not implemented.');
   }
   @ViewChild('editor', { static: true }) editor!: ElementRef<HTMLDivElement>;
   content: string = '';
+  shortcuts: { [key: string]: () => void } = {};
+
 
   ngOnInit(): void {
     this.loadContent();
+    this.autoSave();
   }
 
   format(command: string, value: string = ''): void {
@@ -69,6 +81,9 @@ export class TextEditorComponent implements OnInit {
 
   toggleSuperscript(): void {
     this.format('superscript');
+  }
+  insertLineBreak(): void {
+    this.format('insertHTML', '<br>');
   }
 
   toggleStrikethrough(): void {
@@ -226,8 +241,89 @@ export class TextEditorComponent implements OnInit {
       this.format('backColor', newColor);
     }
   }
+  // Commenting System
+  addComment(text: string): void {
+    const selection = window.getSelection();
+    if (selection && selection.rangeCount > 0) {
+      const range = selection.getRangeAt(0);
+      const comment: Comment = { text, range };
+      this.comments.push(comment);
+    }
+  }
+  downloadAsPDF(): void {
+    html2canvas(this.editor.nativeElement).then((canvas) => {
+      const imgData = canvas.toDataURL('image/png');
+      const pdf = new jsPDF();
+      const imgProps = pdf.getImageProperties(imgData);
+      const pdfWidth = pdf.internal.pageSize.getWidth();
+      const pdfHeight = (imgProps.height * pdfWidth) / imgProps.width;
+      pdf.addImage(imgData, 'PNG', 0, 0, pdfWidth, pdfHeight);
+      pdf.save('content.pdf');
+    });
+  }
+  // Table Insertion and Management
+  insertTable(rows: number, cols: number): void {
+    let table = '<table border="1">';
+    for (let i = 0; i < rows; i++) {
+      table += '<tr>';
+      for (let j = 0; j < cols; j++) {
+        table += '<td>&nbsp;</td>';
+      }
+      table += '</tr>';
+    }
+    table += '</table>';
+    this.format('insertHTML', table);
+  }
+  @HostListener('document:keydown', ['$event'])
+  handleKeyboardEvent(event: KeyboardEvent): void {
+    const keyCombo = `${event.ctrlKey ? 'Ctrl+' : ''}${event.key}`;
+    if (this.shortcuts[keyCombo]) {
+      event.preventDefault();
+      this.shortcuts[keyCombo]();
+    }
+  }
 
-  insertLineBreak(): void {
-    this.format('insertHTML', '<br>');
+  // Grammar and Spell Check
+  checkGrammarAndSpelling(): void {
+    const text = this.editor.nativeElement.innerText;
+    // You may handle grammar and spelling check logic here
+  }
+
+  autoSave(): void {
+    setInterval(() => {
+      this.saveContent();
+    }, 300000);
+  }
+  versionHistory: string[] = [];
+  revertToVersion(index: number): void {
+    if (this.versionHistory[index]) {
+      this.content = this.versionHistory[index];
+      this.editor.nativeElement.innerHTML = this.content;
+    }
+  }
+
+  // Voice Typing
+  startVoiceTyping(): void {
+    const recognition = new webkitSpeechRecognition();
+    recognition.continuous = true;
+    recognition.interimResults = true;
+    recognition.lang = 'en-US';
+
+    recognition.onresult = (event: any) => {
+      let interimTranscript = '';
+      for (let i = event.resultIndex; i < event.results.length; ++i) {
+        if (event.results[i].isFinal) {
+          this.editor.nativeElement.innerHTML += event.results[i][0].transcript;
+        } else {
+          interimTranscript += event.results[i][0].transcript;
+        }
+      }
+    };
+
+    recognition.onerror = (event: any) => {
+      console.error(event.error);
+    };
+
+    recognition.start();
   }
 }
